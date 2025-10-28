fe7f618bc3af9ec62c0313b6dcc06d4f63e6f791
Ramanath <ramanathinst@gmail.com>
2025-10-28 12:22:58 +0530

    10: payments

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,9 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import prisma from "@/lib/db";
+import { polar, checkout, portal, usage, webhooks } from "@polar-sh/better-auth";
+import { polarClient } from "./polar";
+
 
 export const auth = betterAuth({
     database: prismaAdapter(prisma, {
@@ -9,5 +12,24 @@ export const auth = betterAuth({
     emailAndPassword: {
         enabled: true,
         autoSignIn: true
-    }
+    },
+    plugins: [
+        polar({
+            client: polarClient,
+            createCustomerOnSignUp: true,
+            use: [
+                checkout({
+                    products: [
+                        {
+                            productId: "ab2db11a-3fec-4d30-9485-5a72870bb53b",
+                            slug: "Nodebase" // Custom slug for easy reference in Checkout URL, e.g. /checkout/Nodebase
+                        }
+                    ],
+                    successUrl: process.env.POLAR_SUCCESS_URL,
+                    authenticatedUsersOnly: true
+                }),
+                portal()
+            ],
+        })
+    ]
 });
