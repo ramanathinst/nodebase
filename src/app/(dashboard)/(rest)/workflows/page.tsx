b61a6dc87b93575ee33e47bead27699dccbafa57
Ramanath <ramanathinst@gmail.com>
2025-10-29 14:38:45 +0530

    11: workflows crud

--- src/app/(dashboard)/(rest)/workflows/page.tsx
+++ src/app/(dashboard)/(rest)/workflows/page.tsx
@@ -1,9 +1,25 @@
+import { WorkflowsContainer, WorkflowsList } from "@/features/workflows/components/workflows";
+import { prefetchWorkflows } from "@/features/workflows/server/prefetch";
 import { requiredAuth } from "@/lib/auth-utils";
+import { HydrateClient } from "@/trpc/server";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary"
 
 const Page = async() => {
     await requiredAuth();
+
+    prefetchWorkflows();
+
     return <div>
-        Workflows page in ramanath
+        <WorkflowsContainer>
+            <HydrateClient>
+                    <ErrorBoundary fallback={<p>Error!</p>}>
+                        <Suspense fallback={<p>Loading...</p>}>
+                            <WorkflowsList /> 
+                        </Suspense>
+                    </ErrorBoundary>
+            </HydrateClient>
+        </WorkflowsContainer>
     </div>
 }
 
